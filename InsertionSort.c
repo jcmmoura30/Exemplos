3c63442b030c0f185020a94c701f2e70ac36551d
Juan Carlo Monteiro de Moura <jcm.moura30@unifesp.br>
2024-12-11 22:42:26 -0300

    Update InsertionSort.c

--- InsertionSort.c
+++ InsertionSort.c
@@ -10,7 +10,7 @@ void insertionSort(int *vetor, int tamanho) {
         key = vetor[i];
         j = i - 1;
 
-        // Move elements of vetor[0..i-1], that are greater than key, to one position ahead of their current position
+      
         while (j >= 0 && vetor[j] > key) {
             vetor[j + 1] = vetor[j];
             j = j - 1;
@@ -20,23 +20,23 @@ void insertionSort(int *vetor, int tamanho) {
 }
 
 int main() {
-    clock_t t; //variável para armazenar tempo
+    clock_t t; 
     int vetor[TAM];
 
-    //semente de aleatoriedade
-    srand((unsigned)time(NULL)); //o algoritmo tem como semente a hora atual que o código é executado
 
-    //geração aleatório dos valores do vetor
+    srand((unsigned)time(NULL)); 
+
+  
     for(int a = 0; a < TAM; a++)
-        vetor[a] = rand() % TAM; //função rand retorna um número aleatório
+        vetor[a] = rand() % TAM; 
 
-    //Verificando tempo de execução do bubble sort=> t2
-    t = clock(); //armazena tempo, antes de executar a função
-    insertionSort(vetor, TAM); //chama a função
-    t = clock() - t; //tempo final - tempo inicial
+   
+    t = clock();
+    insertionSort(vetor, TAM); 
+    t = clock() - t;
 
-    //imprime o tempo na tela
-    printf("Tempo de execucao: %lf ms\n", ((double)t)/((CLOCKS_PER_SEC/1000))); //conversão para double
+
+    printf("Tempo de execucao: %lf ms\n", ((double)t)/((CLOCKS_PER_SEC/1000))); 
 
     return 0;
 }
